efd9483486d0cfe4f10fc4db79f406809102a35a
Henrique A de Mello <h03mello@gmail.com>
2025-04-23 10:29:32 -0300

    Aula08 - Classes abstratas

---
+++ Aula08- Classes Abstratas/main.py
@@ -0,0 +1,10 @@
+# from Cliente import Cliente
+# c = Cliente("Henrique")
+
+from Fisico import Fisico
+# f = Fisico("Henrique", "000.353.647.97")
+f = Fisico(None)
+f.cadastrar()
+f.limite = 250000
+f.limite = 12500
+f.imprimir()
